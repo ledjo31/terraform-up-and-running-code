0b7a761797891f87e71e1e0185b2b08f5b85a992
ledjo31 <ledjo1990@yahoo.com>
2023-09-20 12:12:17 +0000

    changed AMI and region

--- code/terraform/02-intro-to-terraform-syntax/one-server/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-server/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
   tags = {
